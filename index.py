6afcf5134573f9eb89dfdf0fea58022ec418176a
Pavel Urx <pavel.urx@protonmail.com>
2022-06-03 18:35:53 +0200

    bug update

--- index.py
+++ index.py
@@ -17,13 +17,3 @@ def postdata():
     input_data = json.dumps(request.get_json())
     LpObject.solveForJson(input_data)
     return json.dumps(LpObject.getSolution())
-
-
-
-if __name__ == '__main__':
-    app.secret_key = 'AMkhPWeCFcHT57zf%$j^t&x^ENj8W6Bi'
-
-    sess.init_app(app)
-
-    app.debug = True
-    app.run(port = 80, debug = False)
